3a4af066c9b90abf8340e0851123160afd2e26f7
EduardoVivarL <68909868+EduardoVivarL@users.noreply.github.com>
2023-03-09 23:54:36 -0600

    add new card state to support account games

--- SmartFunRevamp/lib/features/home/provider/cards_state.dart
+++ SmartFunRevamp/lib/features/home/provider/cards_state.dart
@@ -5,5 +5,6 @@ class CardsState with _$CardsState {
   const factory CardsState.initial() = _Initial;
   const factory CardsState.inProgress() = _InProgress;
   const factory CardsState.success(List<AccountCreditPlusDTOList> bonusSummary) = _Success;
+  const factory CardsState.accountGamesSuccess(List<AccountGameDTOList> accountGamesSummary) = _AccountGamesSuccess;
   const factory CardsState.error(String msg) = _Error;
 }

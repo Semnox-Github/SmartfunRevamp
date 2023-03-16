05de44afa40c943ba8625b787c8bcfaa3a582359
EduardoVivarL <68909868+EduardoVivarL@users.noreply.github.com>
2023-03-16 01:52:45 -0600

    card games detail
    +improve elements size
    +add logic for included/excluded games

--- SmartFunRevamp/lib/features/cards_detail/account_games_summary_detail_page.dart
+++ SmartFunRevamp/lib/features/cards_detail/account_games_summary_detail_page.dart
@@ -7,11 +7,17 @@ import 'package:semnox/core/utils/extensions.dart';
 import 'package:semnox/features/cards_detail/bonus_summary_page.dart';
 
 class AccountGamesSummaryDetailPage extends StatelessWidget {
-  const AccountGamesSummaryDetailPage({Key? key, required this.summary}) : super(key: key);
+  AccountGamesSummaryDetailPage({Key? key, required this.summary}) : super(key: key);
   final AccountGameDTOList summary;
+  
+  late final List<AccountGameExtendedDTOList> gamesIncExcDetail = summary.accountGameExtendedDTOList;
+
+  late final List<AccountGameExtendedDTOList> gamesIncluded = gamesIncExcDetail..removeWhere((element) => (!element.exclude));
+  late final List<AccountGameExtendedDTOList> gamesExcluded = gamesIncExcDetail..removeWhere((element) => (element.exclude));
 
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: CustomAppBar(
         title: Column(
@@ -37,13 +43,6 @@ class AccountGamesSummaryDetailPage extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                // Consumer(
-                //   builder: (context, ref, child) {
-                //     return ref.watch(CardsProviders.userGamesSummaryProvider).maybeWhen(
-                //           orElse: () => Container(),
-                //         );
-                //   },
-                // ),
                 Expanded(
                   child: Container(
                     padding: const EdgeInsets.all(10.0),
@@ -91,128 +90,142 @@ class AccountGamesSummaryDetailPage extends StatelessWidget {
               totalBonus: summary.balanceGames.toInt(),
             ),
             const SizedBox(height: 10.0),
-            RichText(
-              textAlign: TextAlign.left,
-              text: const TextSpan(
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                children: [
-                  WidgetSpan(
-                    child: Icon(Icons.check_circle, size: 18, color: Colors.greenAccent),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: RichText(
+                textAlign: TextAlign.left,
+                text: const TextSpan(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                  TextSpan(
-                    text: " Can be used to play below games",
+                  children: [
+                    WidgetSpan(
+                      child: Icon(Icons.check_circle, size: 18, color: Colors.greenAccent),
+                    ),
+                    TextSpan(
+                      text: " Can be used to play below games",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: width * 0.90,
+              child: DataTable(
+                headingRowColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+                  return CustomColors.customOrange; // Use the default value.
+                }),
+                decoration: BoxDecoration(
+                  border: Border.all(color: CustomColors.customLigthGray),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                columns: const <DataColumn>[
+                  DataColumn(
+                    label: Expanded(
+                      child: Text(
+                        'Category',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Expanded(
+                      child: Text(
+                        'Game',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Expanded(
+                      child: Text(
+                        'Qty',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
                   ),
                 ],
-              ),
-            ),
-            DataTable(
-              headingRowColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
-                return CustomColors.customOrange; // Use the default value.
-              }),
-              decoration: BoxDecoration(
-                border: Border.all(color: CustomColors.customLigthGray),
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              columns: const <DataColumn>[
-                DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      'Category',
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ),
-                ),
-                DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      'Game',
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ),
-                ),
-                DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      'Qty',
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ),
-                ),
-              ],
-              rows: const <DataRow>[
-                DataRow(
-                  cells: <DataCell>[
-                    DataCell(Text('All')),
-                    DataCell(Text('All')),
-                    DataCell(Text('100')),
-                  ],
+                rows: List<DataRow>.generate(
+                  gamesIncluded.isEmpty ? 1 : gamesIncluded.length, 
+                  (int index) => DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text(gamesIncluded.isEmpty ? '--' : gamesIncluded[index].gameProfileId.toString())),
+                      DataCell(Text(gamesIncluded.isEmpty ? '--' : gamesIncluded[index].gameId.toString())),
+                      DataCell(Text(gamesIncluded.isEmpty ? '--' : gamesIncluded[index].playLimitPerGame.toString())),
+                    ],
+                  )
                 )
-              ],
+              ),
             ),
-            const SizedBox(height: 10.0),
-            RichText(
-              textAlign: TextAlign.start,
-              text: const TextSpan(
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                children: [
-                  WidgetSpan(
-                    child: Icon(Icons.remove_circle, size: 18, color: Colors.red),
+            const SizedBox(height: 50.0),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: RichText(
+                textAlign: TextAlign.start,
+                text: const TextSpan(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                  TextSpan(
-                    text: " Below games are excluded",
+                  children: [
+                    WidgetSpan(
+                      child: Icon(Icons.remove_circle, size: 18, color: Colors.red),
+                    ),
+                    TextSpan(
+                      text: " Below games are excluded",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: width * 0.90,
+              child: DataTable(
+                headingRowColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+                  return CustomColors.customOrange; // Use the default value.
+                }),
+                decoration: BoxDecoration(
+                  border: Border.all(color: CustomColors.customLigthGray),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                columns: const <DataColumn>[
+                  DataColumn(
+                    label: Expanded(
+                      child: Text(
+                        'Category',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Expanded(
+                      child: Text(
+                        'Game',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Expanded(
+                      child: Text(
+                        'Qty',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ),
                   ),
                 ],
-              ),
-            ),
-            DataTable(
-              headingRowColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
-                return CustomColors.customOrange; // Use the default value.
-              }),
-              decoration: BoxDecoration(
-                border: Border.all(color: CustomColors.customLigthGray),
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              columns: const <DataColumn>[
-                DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      'Category',
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ),
-                ),
-                DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      'Game',
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ),
-                ),
-                DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      'Qty',
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ),
-                ),
-              ],
-              rows: const <DataRow>[
-                DataRow(
-                  cells: <DataCell>[
-                    DataCell(Text('All')),
-                    DataCell(Text('All')),
-                    DataCell(Text('100')),
-                  ],
+                rows: List<DataRow>.generate(
+                  gamesExcluded.isEmpty ? 1 : gamesExcluded.length,
+                  (int index) => DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text(gamesExcluded.isEmpty ? '--' : gamesExcluded[index].gameProfileId.toString())),
+                      DataCell(Text(gamesExcluded.isEmpty ? '--' : gamesExcluded[index].gameId.toString())),
+                      DataCell(Text(gamesExcluded.isEmpty ? '--' : gamesExcluded[index].playLimitPerGame.toString())),
+                    ],
+                  )
                 )
-              ],
+              ),
             ),
           ],
         ),

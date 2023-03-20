967c7754efa33a05c274ba8c59bcb65f2516fd0e
EduardoVivarL <68909868+EduardoVivarL@users.noreply.github.com>
2023-03-19 23:37:45 -0600

    add webview to play page

--- SmartFunRevamp/lib/features/home/view/play_view.dart
+++ SmartFunRevamp/lib/features/home/view/play_view.dart
@@ -1,13 +1,58 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class PlayView extends StatelessWidget {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:semnox/colors/colors.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PlayView extends StatefulWidget {
   const PlayView({Key? key}) : super(key: key);
 
   @override
+  State<PlayView> createState() => _PlayViewState();
+}
+
+class _PlayViewState extends State<PlayView> {
+  
+  final gestureRecognizers = {
+    Factory(() => EagerGestureRecognizer()),
+  };
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Play',
+    
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFCFF8FF),
+        elevation: 0.0,
+        centerTitle: false,
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: const Text(
+          'Play',
+          style: TextStyle(
+            color: CustomColors.customBlue,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        minimum: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.75,
+              child: WebView(
+                gestureRecognizers: gestureRecognizers,
+                initialUrl: "https://google.com",
+                javascriptMode: JavascriptMode.unrestricted,
+              ),
+            ),          
+            const SizedBox(height: 10.0),
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }

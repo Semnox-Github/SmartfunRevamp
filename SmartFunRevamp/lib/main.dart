a18aa68dba7186bf04e167f10b8bd4d8a095b6fa
Mauro Zadunaisky <maurozadu@gmail.com>
2023-05-30 15:07:15 -0300

    force always portrait device orientation #809

--- SmartFunRevamp/lib/main.dart
+++ SmartFunRevamp/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:loading_indicator/loading_indicator.dart';
@@ -46,6 +47,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return ProviderScope(
       child: GlobalLoaderOverlay(
         useDefaultLoading: false,

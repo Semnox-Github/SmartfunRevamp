dea5be71cf3a8291e9d846357091ae3e8709276c
Andres Maldonado <andres.maldonado@grupo-giga.com>
2023-09-04 23:12:51 -0500

    reauthenticated system user

--- SmartFunRevamp/lib/core/api/api_interceptor.dart
+++ SmartFunRevamp/lib/core/api/api_interceptor.dart
@@ -3,9 +3,12 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/instance_manager.dart';
+import 'package:logger/logger.dart';
 
 import 'package:semnox/core/data/datasources/local_data_source.dart';
+import 'package:semnox/core/domain/use_cases/splash_screen/authenticate_base_url_use_case.dart';
 import 'package:semnox/core/routes.dart';
+import 'package:semnox/di/injection_container.dart';
 import 'package:semnox/main.dart';
 
 class AuthorizationInterceptor extends Interceptor {
@@ -25,7 +28,17 @@ class AuthorizationInterceptor extends Interceptor {
     if (err.response?.statusCode == HttpStatus.unauthorized) {
       final localDataSource = Get.find<LocalDataSource>();
       await localDataSource.resetUser();
-      navigatorKey.currentState?.pushNamedAndRemoveUntil(Routes.kLogInPage, ModalRoute.withName(Routes.kLogInPage));
+      final reAuthenticate = Get.find<AuthenticateBaseURLUseCase>();
+      final response = await reAuthenticate();
+      response.fold(
+        (l) {
+          Logger().e(l.message);
+        },
+        (r) {
+          authenticateApi(r, Get.find<String>(tag: 'baseURL'));
+          navigatorKey.currentState?.pushNamedAndRemoveUntil(Routes.kLogInPage, ModalRoute.withName(Routes.kLogInPage));
+        },
+      );
     }
     handler.next(err);
   }

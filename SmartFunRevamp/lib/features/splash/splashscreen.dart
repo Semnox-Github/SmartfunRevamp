3ba5ffccae783bec86cacb80b034ef330e412ce9
EduardoVivarL <68909868+EduardoVivarL@users.noreply.github.com>
2023-09-19 00:27:54 -0600

    Update the splash screen to validate if the there is a new version of the app

--- SmartFunRevamp/lib/features/splash/splashscreen.dart
+++ SmartFunRevamp/lib/features/splash/splashscreen.dart
@@ -1,11 +1,15 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/instance_manager.dart';
 import 'package:semnox/core/api/smart_fun_api.dart';
 import 'package:semnox/core/data/datasources/local_data_source.dart';
+import 'package:semnox/core/domain/entities/config/parafait_defaults_response.dart';
 import 'package:semnox/core/domain/use_cases/authentication/get_execution_context_use_case.dart';
 import 'package:semnox/core/routes.dart';
+import 'package:semnox/core/utils/dialogs.dart';
 import 'package:semnox/core/utils/extensions.dart';
 import 'package:semnox/di/injection_container.dart';
 import 'package:semnox/features/splash/provider/new_splash_screen/new_splash_screen_notifier.dart';
@@ -14,7 +18,8 @@ import 'package:semnox_core/modules/sites/model/site_view_dto.dart';
 
 Future<void> registerLoggedUser(CustomerDTO customerDTO) async {
   final localDataSource = Get.find<LocalDataSource>();
-  final response = await localDataSource.retrieveCustomClass(LocalDataSource.kSelectedSite);
+  final response =
+      await localDataSource.retrieveCustomClass(LocalDataSource.kSelectedSite);
   final getExecutionContextUseCase = Get.find<GetExecutionContextUseCase>();
   final selectedSite = response.fold(
     (l) => null,
@@ -23,7 +28,8 @@ Future<void> registerLoggedUser(CustomerDTO customerDTO) async {
     },
   );
   registerUser(customerDTO);
-  final executionContextResponse = await getExecutionContextUseCase(selectedSite!.siteId!);
+  final executionContextResponse =
+      await getExecutionContextUseCase(selectedSite!.siteId!);
   executionContextResponse.fold(
     (l) {},
     (r) {
@@ -49,26 +55,98 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     final customer = ref.watch(customDTOProvider).valueOrNull;
-    void nextPage() => Navigator.pushReplacementNamed(context, Routes.kAfterSplashScreenPage);
+    void nextPage() =>
+        Navigator.pushReplacementNamed(context, Routes.kAfterSplashScreenPage);
     ref.listen<NewSplashScreenState>(
       newSplashScreenProvider,
       (_, next) {
         next.maybeWhen(
           orElse: () {},
-          success: (cms, langDto, masterSite, parafaitDefaults, needsSiteSelection) {
+          success:
+              (cms, langDto, masterSite, parafaitDefaults, needsSiteSelection) {
             ref.read(newHomePageCMSProvider.notifier).update((_) => cms);
-            ref.read(languangeContainerProvider.notifier).update((_) => langDto);
+            ref
+                .read(languangeContainerProvider.notifier)
+                .update((_) => langDto);
             ref.read(masterSiteProvider.notifier).update((_) => masterSite);
-            ref.read(parafaitDefaultsProvider.notifier).update((_) => parafaitDefaults);
-            if (customer == null) {
-              nextPage();
-            } else {
-              if (needsSiteSelection) {
-                Navigator.pushReplacementNamed(context, Routes.kEnableLocation);
-              } else {
-                registerLoggedUser(customer).then((value) => Navigator.pushReplacementNamed(context, Routes.kHomePage));
-              }
-            }
+            ref
+                .read(parafaitDefaultsProvider.notifier)
+                .update((_) => parafaitDefaults);
+            final parafaitDefault = ref.watch(parafaitDefaultsProvider);
+            //get the update status "O" => optional | "M" => mandatory | other value => not necesary
+            final deprecated = Get.find<String>(tag: 'appVersionDeprecated');
+            final isAndroid = Platform.isAndroid;
+            final isIOS = Platform.isIOS;
+            //get the current date to compare with stored if the update is optional
+            final currentDate = DateTime.now().toIso8601String().split("T")[0];
+            //get the application download url based on the OS
+            final storeUrl = isAndroid
+                ? parafaitDefault
+                        ?.getDefault(ParafaitDefaultsResponse.playStoreUrl) ??
+                    ""
+                : isIOS
+                    ? parafaitDefault?.getDefault(
+                            ParafaitDefaultsResponse.appStoreUrl) ??
+                        ""
+                    : "";
+            //Get the last app update reminder date
+            GluttonLocalDataSource()
+                .retrieveValue(LocalDataSource.kAppUpdateReminderDate)
+                .then((value) async => {
+                      //if application url is not set don't show the update message
+
+                      if (!storeUrl.isNullOrEmpty() &&
+                          //if update is mandatory, show everytime the app starts
+                          (deprecated == "M" ||
+                              //if is optional, show once a day
+                              (deprecated == "O" &&
+                                  currentDate != value.toString())))
+                        {
+                          GluttonLocalDataSource().saveValue(
+                              LocalDataSource.kAppUpdateReminderDate,
+                              currentDate),
+                          await Dialogs.downloadUpdateDialog(context, storeUrl),
+                          if (customer == null)
+                            {
+                              nextPage(),
+                            }
+                          else
+                            {
+                              if (needsSiteSelection)
+                                {
+                                  Navigator.pushReplacementNamed(
+                                      context, Routes.kEnableLocation),
+                                }
+                              else
+                                {
+                                  registerLoggedUser(customer).then((value) =>
+                                      Navigator.pushReplacementNamed(
+                                          context, Routes.kHomePage)),
+                                }
+                            }
+                        }
+                      else
+                        {
+                          if (customer == null)
+                            {
+                              nextPage(),
+                            }
+                          else
+                            {
+                              if (needsSiteSelection)
+                                {
+                                  Navigator.pushReplacementNamed(
+                                      context, Routes.kEnableLocation),
+                                }
+                              else
+                                {
+                                  registerLoggedUser(customer).then((value) =>
+                                      Navigator.pushReplacementNamed(
+                                          context, Routes.kHomePage)),
+                                }
+                            }
+                        }
+                    });
           },
         );
       },
@@ -121,7 +199,8 @@ class SplashScreenImage extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       fit: BoxFit.fill,
-      placeholder: (_, __) => const Center(child: CircularProgressIndicator.adaptive()),
+      placeholder: (_, __) =>
+          const Center(child: CircularProgressIndicator.adaptive()),
       errorWidget: (context, url, error) {
         return Container(
           decoration: const BoxDecoration(

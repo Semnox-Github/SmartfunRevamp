656436cca5d24747f213fd2f3a94be5f4a599666
EduardoVivarL <68909868+EduardoVivarL@users.noreply.github.com>
2023-10-12 00:18:16 -0600

    create image manager widget to handle the images from json in the app

---
+++ SmartFunRevamp/lib/core/widgets/image_manager.dart
@@ -0,0 +1,31 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:semnox/core/utils/extensions.dart';
+import 'package:semnox/features/splash/provider/new_splash_screen/new_splash_screen_notifier.dart';
+
+class ImageManager extends ConsumerWidget {
+  const ImageManager({
+    Key? key,
+    required this.imageKey,
+  }) : super(key: key);
+  final String imageKey;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    String? url = ref.watch(newHomePageCMSProvider)?.cmsImages.toJson().getKey(imageKey);
+    return CachedNetworkImage(
+      imageUrl: url!,
+      placeholder: (_, __) => const Center(child: CircularProgressIndicator.adaptive()),
+      fit: BoxFit.contain,
+      errorWidget: (context, url, error) {
+        return const Center(
+          child: Icon(
+            Icons.error,
+            color: Colors.red,
+            size: 50.0,
+          ),
+        );
+      },
+    );
+  }
+}
